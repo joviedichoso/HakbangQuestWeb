e63e9c05d4301058dd2343ff0c1ecf9746256f21
Jovs <papidichoso.jd@gmail.com>
2025-09-14 23:18:51 +0800

    UPDATED APK

--- src/screens/DashboardScreen.jsx
+++ src/screens/DashboardScreen.jsx
@@ -445,7 +445,7 @@ function DashboardScreen() {
   const handleDownloadAPK = () => {
     setIsDownloading(true);
     const apkUrl =
-      "https://github.com/joviedichoso/HakbangQuestWeb/releases/download/v1.0.0/HakbangQuest.apk";
+      "https://github.com/joviedichoso/HakbangQuest02/releases/download/v1.0.1/HakbangQuest.apk";
     const link = document.createElement("a");
     link.href = apkUrl;
     link.download = "HakbangQuest.apk";
